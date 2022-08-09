df501c3c3baa24013a53564b8406a51f7a94c415
Pedro Emanuel dos Santos Paiva <pedro_lous@hotmail.com>
2022-08-09 17:21:07 -0300

    exercicio node

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,28 @@
+// const idade = Number(process.argv[3]) 
+// console.log("Olá,", process.argv[2],"! Você tem ", (idade), "anos. Em sete anos você terá ", (idade +7), "anos.") 
+
+
+// const operacao = process.argv[2]
+// const number1 = Number(process.argv[3])
+// const number2 = Number(process.argv[4])
+// switch(operacao){
+//     	case "soma":
+// 		console.log(`Resposta: ${number1+number2}`)
+// 		break;
+// 	case "subtracao":
+// 		console.log(`Resposta: ${number1 - number2}`)
+// 		break;
+//     case "multi":
+// 		console.log(`Resposta: ${number1*number2}`)
+// 		break;
+//     case "div":
+//         console.log(`Resposta: ${number1/number2}`)
+//         break;
+// }
+
+
+const tarefas = []
+tarefas[] = process.argv[2]
+console.log(`Tarefa adicionada com sucesso`)
+console.log(tarefas)
+
