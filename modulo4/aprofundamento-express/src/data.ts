f34dd9660fd296ad5ba2021993cf363395c9e5e4
Pedro Emanuel dos Santos Paiva <pedro_lous@hotmail.com>
2022-08-16 23:06:39 -0300

    exercio dia 16

---
+++ modulo4/aprofundamento-express/src/data.ts
@@ -0,0 +1,20 @@
+export const tarefas = [
+    {
+        userId: 1,
+        id: 1,
+        title: "preciso dormir",
+        completed: true,
+    },
+    {
+        userId: 1,
+        id: 2,
+        title: "estudar",
+        completed: true,
+    },
+    {
+        userId: 21,
+        id: 3,
+        title: "ir treinar",
+        completed: false,
+    },
+]
