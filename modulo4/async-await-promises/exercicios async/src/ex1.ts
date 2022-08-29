8e3492f2b975fc204c2be518cebf199db55849cd
Pedro Emanuel dos Santos Paiva <pedro_lous@hotmail.com>
2022-08-29 20:40:00 -0300

    exercicio

---
+++ modulo4/async-await-promises/exercicios async/src/ex1.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { baseURL }  from './baseURL'
+
+
+async function getSubscribers(): Promise<any[]> {
+    const response = await axios.get(`${baseURL}/subscribers`);
+    return response.data;
+  };
